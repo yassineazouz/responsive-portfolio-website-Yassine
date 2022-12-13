0b592fa5c181c45bde8df6d2e8f0f8beaf2aa533
yassineazouz <yassineazzouz30@gmail.com>
2022-12-13 14:57:52 +0100

    repaired qualification and services

--- src/js/main .js
+++ src/js/main .js
@@ -67,19 +67,19 @@ skillsHeader.forEach((eL) => {
 
 /*==================== QUALIFICATION TABS ====================*/
 const tabs = document.querySelectorAll('[data-target]')
-tabContent = document.querySelectorAll('[data-content]')
+const tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target)
-
-        tabContents.forEach(tabContent => {
+ 
+        tabContents.forEach(tabContent =>{
             tabContent.classList.remove('qualification__active')
 
         })
         target.classList.add('qualification__active')
 
-        tabs.forEach((tab) => {
+        tabs.forEach(tab => {
             tab.classList.remove('qualification__active')
         })
         tab.classList.add('qualification__active')
@@ -89,29 +89,29 @@ tabs.forEach(tab => {
 
 
 /*==================== SERVICES MODAL ====================*/
-const modalViews  = document.querySelectorAll('.services__modal')
-      modalBtns   = document.querySelectorAll('.services__button')
-      modalCloses   = document.querySelectorAll('.services__modal-close')
+const modalViews = document.querySelectorAll('.services__modal')
+      modalBtns = document.querySelectorAll('.services__button')
+      modalCloses = document.querySelectorAll('.services__modal-close')
 
 let modal = function(modalClick){
     modalViews[modalClick].classList.add('active-modal')
 }
 
 modalBtns.forEach((modalBtn, i) => {
-    modalBtn.addEventListener('click', () => {
+    modalBtn.addEventListener('click', () =>{
         modal(i)
     })
 })
 
-modalCloses.forEach((modalClose)) => {
-    modalClose.addEventListener('click',() => {
-        modalViews.forEach((modalView) =>{
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
             modalView.classList.remove('active-modal')
         })
     })
-    
 
-}
+
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
